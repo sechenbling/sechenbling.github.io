531854a83c274d6daae2f00baaed277afaf2a694
sechenbling <2040095448@qq.com>
2024-02-01 23:48:26 +0800

    new site building

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230131.003721';
+const cacheName = 'chirpy-20240201.234354';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
